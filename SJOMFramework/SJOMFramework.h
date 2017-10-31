34054409e2a54cb0b3e856030cd6d5f3c4d853df
trung <trung@newwave.vn>
2017-10-31 07:19:19 +0700

    support Carthage

---
+++ SJOMFramework/SJOMFramework.h
@@ -0,0 +1,19 @@
+//
+//  SJOMFramework.h
+//  SJOMFramework
+//
+//  Created by trung on 10/31/17.
+//  Copyright © 2017 nws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SJOMFramework.
+FOUNDATION_EXPORT double SJOMFrameworkVersionNumber;
+
+//! Project version string for SJOMFramework.
+FOUNDATION_EXPORT const unsigned char SJOMFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJOMFramework/PublicHeader.h>
+
+#import <SimpleJSONMapping/SimpleJSONMapping/SimpleJSONObjectMapping.h>
